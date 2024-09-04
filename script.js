a294de0bd79fc401cb752652ecef46b27c690669
RanjanKumar93 <rk0936626@gmail.com>
2024-09-04 21:23:36 +0530

    refactoring

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-// Custom scripts
-// Add any additional JavaScript here
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.getElementById("projects-container");
+  
+    projects.forEach(project => {
+      const projectCard = `
+        <div class="bg-gray-800 p-5 rounded-lg shadow-lg">
+          <a href="${project.link}" target="_blank" class="text-blue-400 hover:underline text-xl font-semibold">${project.title}</a>
+          <p class="text-gray-400 mt-2">${project.description}</p>
+        </div>
+      `;
+      container.innerHTML += projectCard;
+    });
+  });
+  
